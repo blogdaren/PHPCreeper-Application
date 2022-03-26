01e405d2b136cb94bceb48db32103d99c069aa7e
blogdaren <blogdaren@163.com>
2022-03-26 16:51:13 +0800

    add the default spider start script named demo.php

---
+++ demo.php
@@ -0,0 +1,14 @@
+
+<?php
+/**
+ * @script   Baidu.php
+ * @brief    global.start.script
+ * @author   blogdaren<blogdaren@163.com>
+ * @version  1.0.3
+ * @modify   2020-03-19
+ */
+require_once __DIR__ . '/Application/Core/Launcher.php';
+
+\PHPCreeperApp\Core\Launcher::start();
+
+
